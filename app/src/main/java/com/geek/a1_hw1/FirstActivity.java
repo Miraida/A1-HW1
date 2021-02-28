177e25cd7643dbf3dd16cab9d75d2c4fe2e828f3
Miraida <miraidajaanbaeva@gmail.com>
2021-03-01 00:48:46 +0600

    Initial commit

---
+++ app/src/main/java/com/geek/a1_hw1/FirstActivity.java
@@ -0,0 +1,57 @@
+package com.geek.a1_hw1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FirstActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
+    String uriImage ;
+    String textFromSecondActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.firstText);
+        imageView = findViewById(R.id.firstImage);
+    }
+
+
+    public void transitionToSecondActivity(View view) {
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data == null) return;
+
+        uriImage = data.getStringExtra("URI_TEXT");
+        textFromSecondActivity = data.getStringExtra("EDIT_TEXT");
+        imageView.setImageURI(Uri.parse(uriImage));
+        textView.setText(textFromSecondActivity);
+    }
+
+    public void transitionToG_mail(View view) {
+//      Intent intent = new Intent(Intent.ACTION_SENDTO,Uri.fromParts("mailto","miraidajaanbaeva@gmail.com",null));
+//        intent.putExtra(Intent.EXTRA_SUBJECT, "Any subject if you want");
+//        startActivity(intent);
+        Intent send = new Intent(Intent.ACTION_SENDTO);
+        String uriText = "mailto:" + Uri.encode("miraidajaanbaeva@gmail.com") +
+                "?subject=" + Uri.encode("G-mail Intent") +
+                "&body=" + Uri.encode("Hey, I'm from A1-HW1 app");
+        Uri uri = Uri.parse(uriText);
+
+        send.setData(uri);
+        startActivity(Intent.createChooser(send, "Send mail..."));
+    }
+}
